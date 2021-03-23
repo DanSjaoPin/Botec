56fea74e915cdf577027e19793aeec3d7571fab2
DanSjaoPin <64096154+DanSjaoPin@users.noreply.github.com>
2021-03-23 23:19:45 +0300

    Add files via upload

---
+++ cock.py
@@ -0,0 +1,111 @@
+import sqlite3
+from datetime import date
+import random
+from CocksBase import CockUpdate
+
+import vk_api, json
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from config import main_token
+
+vk_session = vk_api.VkApi(token = main_token)
+global vk
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, 203331529)
+
+def sender(id, text):
+	vk_session.method('messages.send', {'chat_id' : id, 'message' : text, 'random_id' : get_random_id()})
+
+base = sqlite3.connect('CocksBase.bd')
+cockBD = base.cursor();
+
+def CockChange(dolbaebID, dolbaebF, dolbaebN, id):
+	cockBD.execute(f"SELECT last_commit_date FROM cock WHERE user_id = '%s'" % (dolbaebID))
+	lastCommitDate = cockBD.fetchone()
+	for simbol in str(lastCommitDate):
+		if simbol == '(' or simbol == ',' or simbol == ')' or simbol == "'":
+			newd = str(lastCommitDate).replace(simbol, '')
+			lastCommitDate = str(newd)
+	if lastCommitDate == str(date.today()):
+		sender(id, '*id%d(%s %s) на сегодня хватит кока, залетай завтра - повторим' % (dolbaebID, dolbaebF, dolbaebN))
+		
+	else:
+		plus = 0
+		chance = random.randrange(100)
+
+		if chance > 0 and chance < 50:
+			plus = random.randrange(10)
+			sender(id, 'Кок *id%d(%s %s) вырос на %d см!' % (dolbaebID, dolbaebF, dolbaebN, plus))
+			
+		elif chance > 50 and chance < 85:
+			plus = random.randrange(10, 15)
+			sender(id, 'Кок *id%d(%s %s) вырос на %d см!' % (dolbaebID, dolbaebF, dolbaebN, plus))
+			
+		elif chance > 85 and chance < 90:
+			plus = random.randrange(15, 20)
+			sender(id, 'Кок *id%d(%s %s) вырос на %d см!' % (dolbaebID, dolbaebF, dolbaebN, plus))
+
+		elif chance > 90 and chance < 99:
+			plus = random.randrange(-15, 0)
+			sender(id, 'Кок *id%d(%s %s) объевреился на %d см!\nСаси' % (dolbaebID, dolbaebF, dolbaebN, plus))
+			
+		elif chance > 0:
+			plus = random.randrange(3)
+			if plus == 1 or plus == 3:
+				sender(id, 'Кок *id%d(%s %s) вырос в 2 раза!' % (dolbaebID, dolbaebF, dolbaebN))
+				plus = 666
+			elif plus == 2:
+				sender(id, 'Кок *id%d(%s %s) отвалился!\nЛох))0)' % (dolbaebID, dolbaebF, dolbaebN))
+				plus = 1488
+		
+		CockUpdate(dolbaebID, dolbaebN, dolbaebF, id, plus)
+		
+		cockBD.execute("SELECT length FROM cock WHERE user_id = '%s'" % (dolbaebID))
+		length = str(cockBD.fetchone())
+		
+		for simbol in length:
+			if simbol == '(' or simbol == ',' or simbol == ')':
+				newlength = length.replace(simbol, '')
+				length = newlength
+				
+		if int(length) >= 0:
+			sender(id, 'Длина кока *id%s(%s %s): %s см' % (dolbaebID, dolbaebF, dolbaebN, length))
+		else: 
+			for simbol in str(length):
+				if simbol == '-':
+					new =  str(length).replace(simbol, '')
+					length = new
+			sender(id, 'Глубина пизды *id%s(%s %s): %s см' % (dolbaebID, dolbaebF, dolbaebN, length))
+		
+def CocksTop(chat_id, id):
+	cockBD.execute("UPDATE cock SET chat_id = %s WHERE user_id = '%s'" % (chat_id, id))
+	base.commit()
+	cockBD.execute("SELECT DolbaebName FROM cock WHERE chat_id = '%s' ORDER BY length DESC" % (chat_id))
+	N = cockBD.fetchall()
+	cockBD.execute("SELECT DolbaebLastName FROM cock WHERE chat_id = '%s' ORDER BY length DESC" % (chat_id))
+	LN = cockBD.fetchall()
+	cockBD.execute("SELECT length FROM cock WHERE chat_id = '%s' ORDER BY length DESC" % (chat_id))
+	L = cockBD.fetchall()
+	i = 0
+	CT = 'Топ коков:\n'
+	
+	for l in L:	
+		for simbol in str(N[i]):
+			if simbol == '(' or simbol == ',' or simbol == ')' or simbol == "'":
+				new =  str(N[i]).replace(simbol, '')
+				N[i] = new
+			
+		for simbol in str(LN[i]):
+			if simbol == '(' or simbol == ',' or simbol == ')' or simbol == "'":
+				new = str(LN[i]).replace(simbol, '')
+				LN[i] = new
+		
+		for simbol in str(L[i]):
+			if simbol == '(' or simbol == ',' or simbol == ')' or simbol == "'":
+				new = str(L[i]).replace(simbol, '')
+				L[i] = new
+			
+		CT += "%s. %s %s —  %s см\n" % (i + 1, N[i], LN[i], L[i])
+		i += 1
+	sender(chat_id, CT)
+	
---
+++ main_bot.py
@@ -0,0 +1,71 @@
+﻿import vk_api, json
+from vk_api.utils import get_random_id
+from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from config import main_token
+from cock import CockChange, CocksTop
+
+vk_session = vk_api.VkApi(token = main_token)
+global vk
+vk = vk_session.get_api()
+longpoll = VkBotLongPoll(vk_session, 203331529)
+
+def sender(id, text):
+	vk_session.method('messages.send', {'chat_id' : id, 'message' : text, 'random_id' : get_random_id()})
+	
+for event in longpoll.listen():	
+	try:			
+		if event.type == VkBotEventType.MESSAGE_NEW:
+			print(event)
+			if event.message.peer_id != event.message.from_id:
+				msg = event.message.text.lower()
+				id = event.chat_id
+				dolbaeb = vk.users.get(user_ids=event.message.from_id)[0]
+				
+				if len(msg) > 1:
+					for simbol in msg:
+						if simbol == ',' or simbol == '.' or simbol == '!' or simbol == '?':
+							newmsg = msg.replace(simbol, '')
+							msg = newmsg
+						
+				if dolbaeb['id'] == 162958234:
+					sender(id, 'Броооооо 🖤')	
+					
+				if msg == 'бот':
+					sender(id, 'Иди нахуй, *id%d(%s %s)' % (dolbaeb['id'], dolbaeb['first_name'], dolbaeb['last_name']))
+					
+				elif msg == 'бот меню':
+					str = ''
+					with open("menu.txt", "r") as menu:
+						for line in menu:
+							str += line
+						sender(id, str)
+					menu.close()				
+					
+				elif msg == 'я':
+					sender(id, 'Головка от хуя')
+					
+				elif msg == 'кок':
+					sender(id, 'кок')
+				
+				elif msg == 'ы':
+					sender(id, 'Гы))0)')
+					
+				elif msg == ':0':
+					sender(id, '0:')
+					
+				elif msg == '0:':
+					sender(id, ':0')
+					
+				elif msg[0] == ')' or msg[-1] == ')':
+					sender(id, '))0)')
+					
+				elif msg == ':с' or msg == ':(' or msg[0] == '(' or msg[-1] == '(' or msg == '😔':
+					sender(id, 'Думай позитивно - стакан всегда наполовину полон, всегда\nТолько не думай что в стакане\nДумай, что в стакане вода')	
+					
+				elif msg == 'бот кок':
+					CockChange(dolbaeb['id'], dolbaeb['first_name'], dolbaeb['last_name'], id)
+					
+				elif msg == 'бот топ коков':
+					CocksTop(id, dolbaeb['id'])
+	except:
+		print("\n--------------------------------------------------------Кракнувса-------------------------------------------------------\n")
