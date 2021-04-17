e790a8b8a2d85276a39ab74915ec95048e503592
DanSjaoPin <64096154+DanSjaoPin@users.noreply.github.com>
2021-04-17 12:19:15 +0300

    Add files via upload

--- roll.py
+++ roll.py
@@ -4,9 +4,9 @@ def Roll():
     dice_value = random.randrange(1, 7)
     dice = ''
     if dice_value == 1:
-        dice = """— — — \n— o — \n— — — """
+        dice = """— — — \n—  o  — \n— — — """
     elif dice_value == 2:
-        dice = """o — — \n— — — \n— — o"""
+        dice = """o  — — \n— — — \n— —  o"""
     elif dice_value == 3:
         dice = """— — o\n— o — \no — — """
     elif dice_value == 4:
