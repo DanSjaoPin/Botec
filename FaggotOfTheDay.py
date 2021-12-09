d62927ed3d85bd25121772476151d1f566a57e2c
Daniil Dymar <dymar02@mail.ru>
2021-12-09 22:21:43 +0300

    Fixed bug in FaggotOfTheDay

--- FaggotOfTheDay.py
+++ FaggotOfTheDay.py
@@ -1,5 +1,6 @@
 import sqlite3
 from datetime import date
+import traceback
 from format import format
 
 from sender import sender
@@ -14,6 +15,9 @@ def FagsCreate():
 
     cockBD = base.cursor()
 
+    cockBD.execute(
+        "CREATE TABLE IF NOT EXISTS faggot(user_id text, DolbaebName text, DolbaebLastName text, chat_id text, date text)")
+
     cockBD.execute("CREATE TABLE IF NOT EXISTS faggot(user_id text PRIMARY KEY, DolbaebName text, DolbaebLastName text, chat_id text, date text)")
 
     base.commit()
@@ -23,23 +27,29 @@ def FagsCreate():
 def WhoIsFaggot(chat_id, degenerat_id):
     base = sqlite3.connect('CocksBase.bd')
     cockBD = base.cursor()
+
     cockBD.execute("UPDATE cock SET chat_id = %s WHERE user_id = '%s'" % (chat_id, degenerat_id))
     cockBD.execute("SELECT user_id FROM faggot WHERE chat_id = '%s'" % (chat_id))
     check = cockBD.fetchone()
+
     if check == None:
         row = ("id", "DolbaebName", "DolbaebLastName", chat_id, "date")
-        cockBD.execute("INSERT INTO faggot VALUES(?, ?, ?, ?, ?)", row)
+        try:
+            cockBD.execute("INSERT INTO faggot VALUES(?, ?, ?, ?, ?)", row)
+        except Exception as e:
+            print('Ошибка:\n', traceback.format_exc())
+            return
         base.commit()
 
     cockBD.execute("SELECT date FROM faggot WHERE chat_id = '%s'" % (chat_id))
     lastCommitDate = cockBD.fetchone()
-    
+
     lastCommitDate = format(lastCommitDate)
 
     if lastCommitDate == str(date.today()):
         cockBD.execute("SELECT user_id FROM faggot WHERE chat_id = '%s'" % (chat_id))
         ID = cockBD.fetchall()
-        
+
         ID = format(ID)
 
         cockBD.execute("SELECT DolbaebName FROM faggot WHERE chat_id = '%s'" % (chat_id))
@@ -65,6 +75,6 @@ def WhoIsFaggot(chat_id, degenerat_id):
         cockBD.execute("UPDATE faggot SET user_id = '%s', DolbaebName = '%s', DolbaebLastName = '%s', date = '%s' WHERE chat_id = '%s'" % (ID, N, LN, date.today(), chat_id))
         base.commit()
         base.close()
-        
+
         WhoIsFaggot(chat_id, degenerat_id)
 
